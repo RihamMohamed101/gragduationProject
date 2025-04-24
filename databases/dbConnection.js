03279bbc277431da62e0cc3376d200c0ea67c975
Riham <moriham190@gmail.com>
2025-04-24 20:21:28 +0200

    changein db

--- databases/dbConnection.js
+++ databases/dbConnection.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose"
 
 
 
-// export const dbConnection = mongoose.connect("mongodb+srv://Riham:0ahCUTj7HjinJ8wI@cluster0.rs75a.mongodb.net/graduationProject").then(() => {
-//       console.log("databaeConnect");
-// })
-
-
-export const dbConnection = mongoose.connect("mongodb://localhost:27017/AlzahaimerSystem").then(() => {
+export const dbConnection = mongoose.connect("mongodb+srv://Riham:0ahCUTj7HjinJ8wI@cluster0.rs75a.mongodb.net/graduationProject").then(() => {
       console.log("databaeConnect");
 })
+
+
+// export const dbConnection = mongoose.connect("mongodb://localhost:27017/AlzahaimerSystem").then(() => {
+//       console.log("databaeConnect");
+// })
